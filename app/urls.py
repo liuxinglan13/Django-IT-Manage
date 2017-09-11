e9ff7c992e6fab2abf9211b963c237142cafbb32
jingkong <87647703@qq.com>
2017-09-11 09:25:16 +0800

    全部使用基于类的视图

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 app_name = 'app'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.host_edit, name='host_edit'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.HostEditView.as_view(), name='host_edit'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.HostDetailView.as_view(), name='host_detail'),
-    url(r'^new/$', views.host_new, name='host_new'),
+    url(r'^new/$', views.HostCreateView.as_view(), name='host_new'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render
 from .models import Host
-from django.shortcuts import get_object_or_404
 from .forms import PostForm
-from django.views.generic import ListView, DetailView
-from django.core.urlresolvers import reverse
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
 
 
-# 首页的视图
+# 基于类的首页视图
 
-def index(request):
-    post_list = Host.objects.all()
-    context = {'post_list': post_list}
-    return render(request, 'app/index.html', context)
+class IndexView(ListView):
+    model = Host
+    context_object_name = 'post_list'
+    template_name = 'app/index.html'
 
 
 # host详情视图
@@ -22,28 +19,17 @@ class HostDetailView(DetailView):
     context_object_name = 'post'
 
 
-# 编辑内容的视图
+# 基于类的 编辑更新数据视图
 
-def host_edit(request, pk):
-    post = get_object_or_404(Host, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            # post = form.save(commit=False)
-            post.save()
-            print("编辑成功")
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'app/host_edit.html', {'form': form})
+class HostEditView(UpdateView):
+    model = Host
+    form_class = PostForm
+    template_name = 'app/host_edit.html'
 
-# 新增新主机信息的视图
 
-def host_new(request):
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-    else:
-        form = PostForm()
-    return render(request, 'app/host_edit.html', {'form': form})
+# 基于类的创建数据视图
+
+class HostCreateView(CreateView):
+    form_class = PostForm
+    template_name = 'app/host_edit.html'
+
