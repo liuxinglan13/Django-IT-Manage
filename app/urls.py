d04d69765ec314995336a5f99f429bec592b4bd0
jingkong <87647703@qq.com>
2017-12-08 10:08:44 +0800

    添加Echart图标

--- app/urls.py
+++ app/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     url(r'^host/(?P<pk>[0-9]+)/edit/$', views.HostEditView.as_view(), name='host_edit'),
     url(r'^host/(?P<pk>[0-9]+)/$', views.HostDetailView.as_view(), name='host_detail'),
     url(r'^new/$', views.HostCreateView.as_view(), name='host_new'),
-    url(r'^host_group/(?P<pk>[0-9]+)/$', views.HostGroupView.as_view(), name='host_group'),
-    url(r'^host_status/(?P<pk>[0-9]+)/$', views.HostStatusView.as_view(), name='host_status'),
-    url(r'^host_category/(?P<pk>[0-9]+)/$', views.HostCategorysView.as_view(), name='host_category'),
+    url(r'^host/(?P<id>[a-z]+)/(?P<pk>[0-9]+)/$', views.HostFilterView.as_view(), name='host_filter'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -16,7 +16,7 @@ class IndexView(LoginRequiredMixin, ListView):
     template_name = 'app/index.html'
 
     def get_queryset(self):
-        return HostComment.objects.all()[0:8:1]
+        return HostComment.objects.all()[0:5:1]
 
 
 # 基于类的首页视图  (访问控制 @login_required 登录后才可访问该视图  基于类的实现)
@@ -71,58 +71,22 @@ class HostCreateView(CreateView):
     form_class = PostForm
     template_name = 'app/host_edit.html'
 
-    # def post(self, request, *args, **kwargs):
-    #     response = super(HostCreateView, self).get(request, *args, **kwargs)
-    #     comment_form = HostCommentForm()
-    #     new_comment = comment_form.save(commit=False)
-    #     new_comment.host = self.object
-    #     new_comment.author = self.request.user
-    #     new_comment.body = '添加主机记录'
-    #     new_comment.save()
-    #     return response
-
-
-# 主机组  视图（点击显示相应主机组下的所有主机）
-class HostGroupView(ListView):
-    # 这一部分和 IndexView 差不多  所有可以 通过 继承来 减少代码 类似 class CategoryView(IndexView):
+# 分组筛选host的类
+# url 接受两个参数 第一个 是 筛选的选项  group status category  第2个参数是 具体筛选的内容的 pk
+class HostFilterView(ListView):
     model = Host
     template_name = 'app/host.html'
     context_object_name = 'post_list'
 
     def get_queryset(self):
-        # 在类视图中，从 URL 捕获的命名组参数值保存在实例的 kwargs 属性（是一个字典）里，
-        # 非命名组参数值保存在实例的 args 属性（是一个列表）里。所以我们使了 self.kwargs.get('pk') 来获取从 URL 捕获的分类 id 值。
-        # 反正 cate 取出来的就是 ID为pk 的一个 Category 实例
-        hostgroup = get_object_or_404(HostGroups, pk=self.kwargs.get('pk'))
-        # 调用父类的get_queryset方法（Host_group 指的是 Host模型中需要 过滤的属性 ）
-        return super(HostGroupView, self).get_queryset().filter(Host_group=hostgroup)
+        id=self.kwargs.get('id')
+        if id == 'group':
+            hostgroup = get_object_or_404(HostGroups, pk=self.kwargs.get('pk'))
+            return super(HostFilterView, self).get_queryset().filter(Host_group=hostgroup)
+        elif id == 'status':
+            host_status = get_object_or_404(Host_status, pk=self.kwargs.get('pk'))
+            return super(HostFilterView, self).get_queryset().filter(host_status=host_status)
+        elif id == 'category':
+            host_category = get_object_or_404(HostCategorys, pk=self.kwargs.get('pk'))
+            return super(HostFilterView, self).get_queryset().filter(Host_category=host_category)
 
-# 主机状态
-class HostStatusView(ListView):
-    # 这一部分和 IndexView 差不多  所有可以 通过 继承来 减少代码 类似 class CategoryView(IndexView):
-    model = Host
-    template_name = 'app/host.html'
-    context_object_name = 'post_list'
-
-    def get_queryset(self):
-        # 在类视图中，从 URL 捕获的命名组参数值保存在实例的 kwargs 属性（是一个字典）里，
-        # 非命名组参数值保存在实例的 args 属性（是一个列表）里。所以我们使了 self.kwargs.get('pk') 来获取从 URL 捕获的分类 id 值。
-        # 反正 cate 取出来的就是 ID为pk 的一个 Category 实例
-        host_status = get_object_or_404(Host_status, pk=self.kwargs.get('pk'))
-        # 调用父类的get_queryset方法（Host_group 指的是 Host模型中需要 过滤的属性 ）
-        return super(HostStatusView, self).get_queryset().filter(host_status=host_status)
-
-# 主机分类
-class HostCategorysView(ListView):
-    # 这一部分和 IndexView 差不多  所有可以 通过 继承来 减少代码 类似 class CategoryView(IndexView):
-    model = Host
-    template_name = 'app/host.html'
-    context_object_name = 'post_list'
-
-    def get_queryset(self):
-        # 在类视图中，从 URL 捕获的命名组参数值保存在实例的 kwargs 属性（是一个字典）里，
-        # 非命名组参数值保存在实例的 args 属性（是一个列表）里。所以我们使了 self.kwargs.get('pk') 来获取从 URL 捕获的分类 id 值。
-        # 反正 cate 取出来的就是 ID为pk 的一个 Category 实例
-        host_category = get_object_or_404(HostCategorys, pk=self.kwargs.get('pk'))
-        # 调用父类的get_queryset方法（Host_group 指的是 Host模型中需要 过滤的属性 ）
-        return super(HostCategorysView, self).get_queryset().filter(Host_category=host_category)
